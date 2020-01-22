6b258beb28dbc4859372842f9bd5c606182b814c
siskut7117 <siskuts@gmail.com>
2020-01-22 16:53:56 +0700

    Modify makeHttpObject function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Store the URL into variable 
+// // Store the URL into variable 
 var url =  window.location.href; 
       
-// Created a URL object using URL() method 
+// // Created a URL object using URL() method 
 var parser = new URL(url);
 
 var hn = parser.hostname.split('.').reverse();
@@ -9,32 +9,27 @@ var host = hn[1] + '.' + hn[0];
 var subdomain = '';
 var getsubdomain = url.split("/")[2].split(".")[0];
 if(getsubdomain == 'www'){
-    subdomain = '';
-} else {
-    subdomain = getsubdomain;
-}
+     subdomain = '';
+ } else {
+     subdomain = getsubdomain;
+ }
 
 function makeHttpObject() {
     try {return new XMLHttpRequest();}
     catch (error) {}
-    try {return new ActiveXObject("Msxml2.XMLHTTP");}
-    catch (error) {}
-    try {return new ActiveXObject("Microsoft.XMLHTTP");}
-    catch (error) {}
-  
+
     throw new Error("Could not create HTTP request object.");
   }
   var request = makeHttpObject();
-  request.open("GET", "your_url", true);
+  request.open("GET", url);
   request.send(null);
   request.onreadystatechange = function() {
     if (request.readyState == 4)
       alert(
         "URL : " + url + ", " +
         "protocol : " + parser.protocol + ", " +
-        "host : " + parser.host + ", " +
+        "hostname : " + parser.host + ", " +
         "port : " + parser.port + ", " +
-        "hostname : " + parser.hostname + ", "+
         "domain : " + host + ", " +
         "subdomain : " + subdomain + "," +
         request.responseText
